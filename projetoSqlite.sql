5e23da8bac2b8ee9e0cac0e688d433df194cef5f
[Angelica] <91993343+Angelica0216392420302112001Nieves@users.noreply.github.com>
2021-11-14 20:59:00 -0300

    Adicionando as consultas

--- projetoSqlite.sql
+++ projetoSqlite.sql
@@ -1091,75 +1091,24 @@ VALUES
         (1, 3, 863.53),
         (2, 29, 291.42);
 
+        -- Quem são os clientes que tem dependentes --
+
+SELECT id_cliente AS Cliente, id_conta AS Conta, dependente AS Dependente
+FROM cliente_conta
+WHERE (id_cliente IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35)
+AND dependente > 0)
 
 
---DQL - Seleciona todos os clientes 
-SELECT id, nome, cpf
-FROM cliente;
+-- Os que menos fizeram transações
+SELECT 
+MIN(id) AS'MENORES TRANSAÇÕES',id_cliente_conta AS 'ID DA CONTA DO CLIENTE'
+from transacao trs
+group by id_cliente_conta
+order by id desc limit 5;
 
---conta
-SELECT id, numero, saldo 
-FROM conta;
-
---cliente_conta
-SELECT id, id_cliente, id_conta, dependente 
-FROM cliente_conta;
-
---tipo_transacao
-SELECT id, descricao
-FROM tipo_transacao;
-
---transacao
-SELECT id, id_tipo_transacao, id_cliente_conta, valor
-FROM transacao;
-
-SELECT id
-FROM cliente
-WHERE
-    nome LIKE 'Kau%' ;
-    
-
- 
-SELECT id, id_tipo_transacao, id_cliente_conta, valor
-FROM transacao
-WHERE
-    (id_cliente_conta IN (1,2,3,4,5)
-    AND valor > 5000)
-    OR (id_cliente_conta IN (31,32,33,34,35)
-    AND valor < 50);
-    
-
-
-SELECT
-    id,
-    COUNT(id) AS "Quantidade",
-    SUM(valor) AS "Soma",
-    MAX(valor) AS "Máximo",
-    MIN(valor) AS "Mínimo",
-    AVG(valor) AS "Média"
-FROM transacao
-GROUP BY id_tipo_transacao
-ORDER BY 1;
-
-SELECT
-    transacao.id_tipo_transacao,
-    descricao,
-    COUNT(transacao.id) AS "Quantidade",
-    SUM(valor) AS "Soma",
-    MAX(valor) AS "Máximo",
-    MIN(valor) AS "Mínimo",
-    AVG(valor) AS "Média"
-FROM transacao
-JOIN tipo_transacao
-	ON transacao.id_tipo_transacao = tipo_transacao.id
-GROUP BY transacao.id_tipo_transacao, descricao
-ORDER BY 1;
-
-
-
-SELECT
-    id, valor
-FROM
-    transacao
-ORDER BY id DESC
-LIMIT 5;
+-- Os que mais fizeram transações
+SELECT 
+MAX(id) AS'MAIORES TRANSAÇÕES',id_cliente_conta AS 'ID DA CONTA DO CLIENTE'
+from transacao trs
+group by id_cliente_conta
+order by id desc limit 5;
